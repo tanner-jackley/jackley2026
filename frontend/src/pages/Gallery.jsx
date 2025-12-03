20fb82222b3ca0dc9fa20b0b3b725cf72689ef98
tannerjackley <s561664@nwmissouri.edu>
2025-12-03 16:31:33 -0600

    map all images to gallery from folder

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -2,6 +2,8 @@ import "../css/App.css";
 import "../css/bulma.css";
 import "../css/index.css";
 
+const images = import.meta.glob("/src/assets/gallery/*.jpg", { eager: true });
+
 function Gallery() {
   return (
     <>
@@ -11,23 +13,9 @@ function Gallery() {
           <p>Photography by Ruth Loftis</p>
         </div>
         <div className="gallery">
-          <img src="src/assets/gallery/T99A6531.jpg" alt="T99A6531" />
-          <img src="src/assets/gallery/T99A6585.jpg" alt="T99A6585" />
-          <img src="src/assets/gallery/T99A6589.jpg" alt="T99A6589" />
-          <img src="src/assets/gallery/T99A6617.jpg" alt="T99A6617" />
-          <img src="src/assets/gallery/T99A6645.jpg" alt="T99A6645" />
-          <img src="src/assets/gallery/T99A6694.jpg" alt="T99A6694" />
-          <img src="src/assets/gallery/T99A6843.jpg" alt="T99A6843" />
-          <img src="src/assets/gallery/T99A6972.jpg" alt="T99A6972" />
-          <img src="src/assets/gallery/T99A7052.jpg" alt="T99A7052" />
-          <img src="src/assets/gallery/T99A7082.jpg" alt="T99A7082" />
-          <img src="src/assets/gallery/T99A7094.jpg" alt="T99A7094" />
-          <img src="src/assets/gallery/T99A7493.jpg" alt="T99A7493" />
-          <img src="src/assets/gallery/T99A7648.jpg" alt="T99A7648" />
-          <img src="src/assets/gallery/T99A7902.jpg" alt="T99A7902" />
-          <img src="src/assets/gallery/T99A7932.jpg" alt="T99A7932" />
-          <img src="src/assets/gallery/T99A7950.jpg" alt="T99A7950" />
-          <img src="src/assets/gallery/T99A7973.jpg" alt="T99A7973" />
+          {Object.values(images).map((img, i) => (
+            <img key={i} src={img.default} alt={`gallery-${i}`} />
+          ))}
         </div>
       </section>
     </>
